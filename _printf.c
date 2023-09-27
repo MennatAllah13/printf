a9080ea264994853bfdb3cfd25265425e8922925
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-27 05:19:17 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,46 +2,46 @@
 
 /**
  * print_f - form output conversion and print data.
- * @form: input string.
+ * @format: input string.
  * Return: number of char printed.
  */
-int print_f(const char *form, ...)
+int print_f(const char *format, ...)
 {
 	unsigned int i = 0, len = 0, ind = 0;
 	va_list arg;
 	int (*func)(va_list, char *, unsigned int);
 	char *buffer;
 
-	va_start(arg, form), buffer = malloc(sizeof(char) * 1024);
-	if (!form || !buffer || (!form[i + 1] && form[i] == '%'))
+	va_start(arg, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (!format[i + 1] && format[i] == '%'))
 		return (-1);
-	if (!form[i])
+	if (!format[i])
 		return (0);
-	for (i = 0; form && form[i]; i++)
+	for (i = 0; format && format[i]; i++)
 	{
-		if (form[i] == '%')
+		if (format[i] == '%')
 		{
-			if (form[i + 1] == '\0')
+			if (format[i + 1] == '\0')
 			{	buff_print(buffer, ind), free(buffer), va_end(arg);
 				return (-1);
 			}
 			else
-			{	func = get_func(form, i + 1);
+			{	func = get_func(format, i + 1);
 				if (!func)
 				{
-					if (form[i + 1] == ' ' && !form[i + 2])
+					if (format[i + 1] == ' ' && !format[i + 2])
 						return (-1);
-					buff_handler(buffer, form[i], ind), len++, i--;
+					buff_handler(buffer, format[i], ind), len++, i--;
 				}
 				else
 				{
 					len += func(arg, buffer, ind);
-					i += num_id(form, i + 1);
+					i += num_id(format, i + 1);
 				}
 			} i++;
 		}
 		else
-			buff_handler(buffer, form[i], ind), len++;
+			buff_handler(buffer, format[i], ind), len++;
 		for (ind = len; ind > 1024; ind -= 1024)
 			;
 	}
